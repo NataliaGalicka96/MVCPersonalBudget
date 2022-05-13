bf74d27f25590d67952cd96f32c745709797db2b
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-13 18:26:45 +0200

    Provide the isLoggedIn() method in the Twij template (Core/View.php)

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use \Core\View;
 use \App\Models\User;
+use \App\Auth;
 
 /**
  * Login controller
@@ -37,12 +38,10 @@ use \App\Models\User;
 
          if($user){
 
-            session_regenerate_id(true);
-
-            $_SESSION['user_id']=$user->id;
+            Auth::login($user);
 
             $this->redirect('/');
-            
+
          }else{
              View::renderTemplate('Login/new.html',[
                  'email'=>$_POST['email']
@@ -55,29 +54,7 @@ use \App\Models\User;
 
      public function destroyAction()
      {
-         // Unset all of the session variables.
-        $_SESSION = array();
- 
-        // If it's desired to kill the session, also delete the session cookie.
-        // Note: This will destroy the session, and not just the session data!
-        if (ini_get("session.use_cookies")) {
-            $params = session_get_cookie_params();
-           
-            setcookie(
-                session_name(),
-                 '',
-                  time() - 42000,
-                $params["path"],
-                 $params["domain"],
-                $params["secure"],
-                 $params["httponly"]
-            );
-        }
- 
-        // Finally, destroy the session.
-        session_destroy();
-
-
+        Auth::logout();
         $this->redirect('/');
      }
  }
