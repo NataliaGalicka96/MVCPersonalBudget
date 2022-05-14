7a9e0523d4a08c186aee23db0e636a33c5df4f1b
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-14 20:05:15 +0200

    Add new controller Password and add new view forgot.html in password folder

---
+++ App/Controllers/Password.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Password controller
+ * 
+ */
+
+ class Password extends \Core\Controller
+ {
+      /**
+     * Show the forgotten password page
+     *
+     * @return void
+     */
+    public function forgotAction()
+    {
+        
+    }
+
+ }
