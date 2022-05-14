e76cc737f34c30060f64ee9e4999ae9ee7e1c842
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-14 19:52:21 +0200

    Add new class Mail and create send method

---
+++ App/Mail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+class Mail
+{
+   /**
+     * Send a message
+     *
+     * @param string $to Recipient
+     * @param string $subject Subject
+     * @param string $text Text-only content of the message
+     * @param string $html HTML content of the message
+     *
+     * @return mixed
+     */
+    public static function send($to, $subject, $text, $html)
+    {
+    }
+}
+ 
+}
