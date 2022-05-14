df40334e7a0e6f604e54ce10ff4d609a98cda4da
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-14 06:46:42 +0200

    Add new method getUser() in Auth class which gets the current logged-in user

--- App/Controllers/Items.php
+++ App/Controllers/Items.php
@@ -3,19 +3,28 @@
 namespace App\Controllers;
 
 use \Core\View;
-use \App\Auth;
+
 
 /**
  * Items controller (example)
  *
  * PHP version 7.0
  */
+//class Items extends \Core\Controller
 class Items extends Authenticated
 {
- 
 
-
-
+    /**
+     * Require the user to be authenticated before giving access to all methods in the controller
+     *
+     * @return void
+     */
+    /*
+    protected function before()
+    {
+        $this->requireLogin();
+    }
+    */
 
     /**
      * Items index
@@ -24,11 +33,9 @@ class Items extends Authenticated
      */
     public function indexAction()
     {
-
         View::renderTemplate('Items/index.html');
-
     }
-    
+
     /**
      * Add a new item
      *
@@ -38,7 +45,7 @@ class Items extends Authenticated
     {
         echo "new action";
     }
- 
+
     /**
      * Show an item
      *
@@ -49,5 +56,4 @@ class Items extends Authenticated
         echo "show action";
     }
 }
-
 
--- Core/Controller.php
+++ Core/Controller.php
@@ -2,6 +2,8 @@
 
 namespace Core;
 
+use App\Auth;
+
 /**
  * Base controller
  *
