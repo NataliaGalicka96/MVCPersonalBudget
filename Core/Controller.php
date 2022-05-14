acb92a3a633f031bfc568d5784ef46f06ca93424
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-14 06:18:33 +0200

    Create requireLogin() method in Core/Controller.php, which checks that user is logged in or not logged in. Call this method in Items controller in indexAction()

--- Core/Controller.php
+++ Core/Controller.php
@@ -77,4 +77,22 @@ abstract class Controller
         exit;
     }
 
+    /**
+     * Require the user to be logged in before giving access to the requested page.
+     * Remember the requested page for later, then redirect to the login page.
+     *
+     * @return void
+     */
+    public function requireLogin()
+    {
+        if (! Auth::isLoggedIn()) {
+ 
+            Auth::rememberRequestedPage();
+ 
+            $this->redirect('/login');
+        }
+    }
+
+
+
 }
