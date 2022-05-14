556299d598a3cc18446e7e32db2048378eb9d598
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-14 09:50:53 +0200

    Add addMessage() method in Flash

--- App/Flash.php
+++ App/Flash.php
@@ -10,12 +10,34 @@ namespace App;
  */
 class Flash
 {
-    /**
+        /**
      * Add a message
      *
      * @param string $message  The message content
+     * @param string $type  The optional message type, defaults to SUCCESS
      *
      * @return void
+     */
+    //public static function addMessage($message)
+    public static function addMessage($message, $type = 'success')
+    {
+        // Create array in the session if it doesn't already exist
+        if (! isset($_SESSION['flash_notifications'])) {
+            $_SESSION['flash_notifications'] = [];
+        }
+
+        // Append the message to the array
+        //$_SESSION['flash_notifications'][] = $message;
+        $_SESSION['flash_notifications'][] = [
+            'body' => $message,
+            'type' => $type
+        ];
+    }
+
+    /**
+     * Get all the messages
+     *
+     * @return mixed  An array with all the messages or null if none set
      */
     public static function getMessages()
     {
@@ -23,7 +45,7 @@ class Flash
             //return $_SESSION['flash_notifications'];
             $messages = $_SESSION['flash_notifications'];
             unset($_SESSION['flash_notifications']);
- 
+
             return $messages;
         }
     }
