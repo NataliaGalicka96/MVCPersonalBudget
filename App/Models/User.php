bfe053a2d5c0309b792991232a70ae9ce28c5f46
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-14 17:34:01 +0200

    Forget the remember login when logging out

--- App/Models/User.php
+++ App/Models/User.php
@@ -187,7 +187,7 @@ class User extends \Core\Model
      }
 
      
-/**
+    /**
      * Remember the login by inserting a new unique token into the remembered_logins table
      * for this user record
      *
@@ -196,24 +196,22 @@ class User extends \Core\Model
     public function rememberLogin()
     {
         $token = new Token();
-        $hashed_token = $token ->getHash();
-        $this->remember_token=$token->getValue();
+        $hashed_token = $token->getHash();
+        $this->remember_token = $token->getValue();
 
         $this->expiry_timestamp = time() + 60 * 60 * 24 * 30;  // 30 days from now
 
-        $sql = 'INSERT INTO remembered_logins(token_hash, user_id, expires_at)
-        VALUES (:token_hash, :user_id, :expires_at)';
+        $sql = 'INSERT INTO remembered_logins (token_hash, user_id, expires_at)
+                VALUES (:token_hash, :user_id, :expires_at)';
 
         $db = static::getDB();
         $stmt = $db->prepare($sql);
 
         $stmt->bindValue(':token_hash', $hashed_token, PDO::PARAM_STR);
         $stmt->bindValue(':user_id', $this->id, PDO::PARAM_INT);
-        $stmt->bindValue('expires_at', date('Y-m-d H:i:s', $this->expiry_timestamp), PDO::PARAM_STR);
-
+        $stmt->bindValue(':expires_at', date('Y-m-d H:i:s', $this->expiry_timestamp), PDO::PARAM_STR);
 
         return $stmt->execute();
-    
     }
 
   }
