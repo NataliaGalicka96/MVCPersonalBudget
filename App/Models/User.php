bde11988a8e5a85697c8a5988b0a77c51d976ddb
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-15 13:01:27 +0200

    Use getTemplate() method in User Model sendPasswordResetEmail()

--- App/Models/User.php
+++ App/Models/User.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use PDO;
-use \Core\View;
 use \App\Token;
+use \App\Mail;
+use \Core\View;
 
 
 /**
@@ -213,6 +214,73 @@ class User extends \Core\Model
 
         return $stmt->execute();
     }
+
+/**
+     * Send password reset instructions to the user specified
+     *
+     * @param string $email The email address
+     *
+     * @return void
+     */
+    public static function sendPasswordReset($email)
+    {
+        $user = static::findByEmail($email);
+ 
+        if ($user) {
+ 
+            if ($user->startPasswordReset()) {
+ 
+                $user->sendPasswordResetEmail();
+ 
+            }
+        }
+    }
+
+
+    /**
+     * Start the password reset process by generating a new token and expiry
+     *
+     * @return void
+     */
+    protected function startPasswordReset()
+    {
+        $token = new Token();
+        $hashed_token = $token->getHash();
+        $this->password_reset_token = $token->getValue();
+
+        $expiry_timestamp = time() + 60 * 60 * 2;  // 2 hours from now
+
+        $sql = 'UPDATE users
+                SET password_reset_hash = :token_hash,
+                    password_reset_expires_at = :expires_at
+                WHERE id = :id';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':token_hash', $hashed_token, PDO::PARAM_STR);
+        $stmt->bindValue(':expires_at', date('Y-m-d H:i:s', $expiry_timestamp), PDO::PARAM_STR);
+        $stmt->bindValue(':id', $this->id, PDO::PARAM_INT);
+
+        return $stmt->execute();
+    }
+
+    //**
+    * Send password reset instructions in an email to the user
+    *
+    * @return void
+    */
+   protected function sendPasswordResetEmail()
+   {
+       $url = 'http://' . $_SERVER['HTTP_HOST'] . '/password/reset/' . $this->password_reset_token;
+
+       $text = View::getTemplate('Password/reset_email.txt', ['url' => $url]);
+       $html = View::getTemplate('Password/reset_email.html', ['url' => $url]);
+
+       Mail::send($this->email, 'Password reset', $text, $html);
+   }
+
+
 
   }
 
