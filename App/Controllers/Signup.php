8d9987139159c6f9ea1cb8c13eafdc0524f898f4
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-15 14:52:48 +0200

    Add activateAction() method in Signup controller

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -57,5 +57,16 @@ class Signup extends \Core\Controller
         View::renderTemplate('Signup/success.html');
     }
 
+    /**
+     * Activate a new account
+     *
+     * @return void
+     */
+    public function activateAction()
+    {
+      $this->route_params['token']);      
+    }
+
+
 
 }
