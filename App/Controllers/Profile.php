1340a83a5f2d178a989f912c5889fb69ba101cfa
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-16 09:47:13 +0200

    Add showAction() method in Profile controller which render Template show.html

---
+++ App/Controllers/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+use Core/View;
+
+/**
+ * Profile controller
+ *
+ * PHP version 7.0
+ */
+
+
+ class Profile extends Core\Controller
+ {
+     /**
+      * Show the profile 
+      *
+      *@return void
+      */
+
+      public function showAction()
+      {
+          View::renderTemplate('Profile/show.html');
+      }
+}
