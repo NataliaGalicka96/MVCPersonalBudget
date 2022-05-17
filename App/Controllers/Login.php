9a5fdd20f0243a1b0e7bc4b8abc427ab623db561
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-17 13:52:29 +0200

    Small change in Login controller

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -16,53 +16,59 @@ class Login extends \Core\Controller
      *
      * @return void
      */
-    public function newAction()
-    {
-        View::renderTemplate('Login/new.html');
-    }
+
+        public function newAction()
+        {
+            if(Auth::getLoggedUser()){
+                $this->redirect(Auth::getReturnToPage());
+            }
+
+            View::renderTemplate('Login/new.html');
+        }
  
     /**
      * Log in a user
      *
      * @return void
      */
-    public function createAction()
-    {
-        $user = User::authenticate($_POST['email'], $_POST['password']);
 
-        $remember_me = isset($_POST['remember_me']);
+        public function createAction()
+        {
+            $user = User::authenticate($_POST['email'], $_POST['password']);
 
+            $remember_me = isset($_POST['remember_me']);
 
-        if ($user) {
- 
-            Auth::login($user, $remember_me);
- 
-            Flash::addMessage('Login successful', Flash::SUCCESS);
- 
-            $this->redirect(Auth::getReturnToPage());
- 
-        } else {
- 
-            Flash::addMessage('Login unsuccessful, please try again', Flash::WARNING);
- 
-            View::renderTemplate('Login/new.html', [
-                'email' => $_POST['email'],
-                'remember_me' => $remember_me
-            ]);
+            if ($user) {
+    
+                Auth::login($user, $remember_me);
+    
+                Flash::addMessage('Login successful', Flash::SUCCESS);
+    
+                $this->redirect(Auth::getReturnToPage());
+    
+            } else {
+    
+                Flash::addMessage('Login unsuccessful, please try again', Flash::WARNING);
+    
+                View::renderTemplate('Login/new.html', [
+                    'email' => $_POST['email'],
+                    'remember_me' => $remember_me
+                ]);
+            }
         }
-    }
  
     /**
      * Log out a user
      *
      * @return void
      */
-    public function destroyAction()
-    {
-        Auth::logout();
- 
-        $this->redirect('/login/show-logout-message');
-    }
+
+        public function destroyAction()
+        {
+            Auth::logout();
+    
+            $this->redirect('/login/show-logout-message');
+        }
  
     /**
      * Show a "logged out" flash message and redirect to the homepage. Necessary to use the flash messages
@@ -71,12 +77,13 @@ class Login extends \Core\Controller
      *
      * @return void
      */
-    public function showLogoutMessageAction()
-    {
-      Flash::addMessage('Logout successful', Flash::SUCCESS);
- 
-      $this->redirect('/');
-    }
+
+        public function showLogoutMessageAction()
+        {
+            Flash::addMessage('You\'ve successfully logged out.', Flash::SUCCESS);
+        
+            $this->redirect('/');
+        }
 
 }
  
