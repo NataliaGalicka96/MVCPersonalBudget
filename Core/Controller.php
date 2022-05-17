8b3c3f613cc75fcd14a48e6da9620dc1e5d2ab0f
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-17 13:00:26 +0200

    Refactor code in Controller.php

--- Core/Controller.php
+++ Core/Controller.php
@@ -26,10 +26,10 @@ abstract class Controller
      *
      * @return void
      */
-    public function __construct($route_params)
-    {
-        $this->route_params = $route_params;
-    }
+
+        public function __construct($route_params){
+            $this->route_params = $route_params;
+        }
 
     /**
      * Magic method called when a non-existent or inaccessible method is
@@ -42,43 +42,46 @@ abstract class Controller
      *
      * @return void
      */
-    public function __call($name, $args)
-    {
-        $method = $name . 'Action';
 
-        if (method_exists($this, $method)) {
-            if ($this->before() !== false) {
-                call_user_func_array([$this, $method], $args);
-                $this->after();
+        public function __call($name, $args){
+            $method = $name . 'Action';
+
+            if (method_exists($this, $method)) {
+                if ($this->before() !== false) {
+                    call_user_func_array([$this, $method], $args);
+                    $this->after();
+                }
+            } else {
+                throw new \Exception("Method $method not found in controller " . get_class($this));
             }
-        } else {
-            throw new \Exception("Method $method not found in controller " . get_class($this));
         }
-    }
 
     /**
      * Before filter - called before an action method.
      *
      * @return void
      */
-    protected function before()
-    {
-    }
+
+        protected function before()
+        {
+        }
 
     /**
      * After filter - called after an action method.
      *
      * @return void
      */
-    protected function after()
-    {
-    }
 
-    public function redirect($url)
-    {
-        header('Location: http://' . $_SERVER['HTTP_HOST'] . $url, true, 303);
-        exit;
-    }
+        protected function after()
+        {
+        }
+
+
+        public function redirect($url)
+        {
+            header('Location: http://' . $_SERVER['HTTP_HOST'] . $url, true, 303);
+            exit;
+        }
 
     /**
      * Require the user to be logged in before giving access to the requested page.
@@ -86,19 +89,19 @@ abstract class Controller
      *
      * @return void
      */
-    public function requireLogin()
-    {
-        if (! Auth::getUser()) {
 
-            Flash::addMessage('Please login to access that page', Flash::INFO);
- 
-            Auth::rememberRequestedPage();
- 
-            $this->redirect('/login');
+        public function requireLogin(){
+            if (! Auth::getUser()) {
 
-            
+                Flash::addMessage('Please log in to access that page', Flash::INFO);
+    
+                Auth::rememberRequestedPage();
+    
+                $this->redirect('/login');
+
+                
+            }
         }
-    }
 
 
 
