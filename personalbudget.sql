3fb32a918c2ea47581d8297481ac8ed35dd15d06
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-18 16:40:10 +0200

    Add new file income.css

---
+++ personalbudget.sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 29 Cze 2018, 10:55
+-- Wersja serwera: 10.1.30-MariaDB
+-- Wersja PHP: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `personal_budget`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expenses`
+--
+
+CREATE TABLE `expenses` (
+  `id` int(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `expense_category_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `payment_method_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `amount` decimal(8,2) NOT NULL DEFAULT '0.00',
+  `date_of_expense` date NOT NULL,
+  `expense_comment` varchar(100) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expenses_category_assigned_to_users`
+--
+
+CREATE TABLE `expenses_category_assigned_to_users` (
+  `id` int(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expenses_category_default`
+--
+
+CREATE TABLE `expenses_category_default` (
+  `id` int(11) UNSIGNED NOT NULL PRIMARY KEY,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `expenses_category_default`
+--
+
+INSERT INTO `expenses_category_default` (`id`, `name`) VALUES
+(1, 'Transport'),
+(2, 'Books'),
+(3, 'Food'),
+(4, 'Apartments'),
+(5, 'Telecommunication'),
+(6, 'Health'),
+(7, 'Clothes'),
+(8, 'Hygiene'),
+(9, 'Kids'),
+(10, 'Recreation'),
+(11, 'Trip'),
+(12, 'Savings'),
+(13, 'For Retirement'),
+(14, 'Debt Repayment'),
+(15, 'Gift'),
+(16, 'Another');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `incomes`
+--
+
+CREATE TABLE `incomes` (
+  `id` int(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `income_category_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `amount` decimal(8,2) NOT NULL DEFAULT '0.00',
+  `date_of_income` date NOT NULL,
+  `income_comment` varchar(100) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `incomes_category_assigned_to_users`
+--
+
+CREATE TABLE `incomes_category_assigned_to_users` (
+  `id` int(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `incomes_category_default`
+--
+
+CREATE TABLE `incomes_category_default` (
+  `id` int(11) UNSIGNED NOT NULL PRIMARY KEY,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `incomes_category_default`
+--
+
+INSERT INTO `incomes_category_default` (`id`, `name`) VALUES
+(1, 'Salary'),
+(2, 'Interest'),
+(3, 'Allegro'),
+(4, 'Another');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `payment_methods_assigned_to_users`
+--
+
+CREATE TABLE `payment_methods_assigned_to_users` (
+  `id` int(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `payment_methods_default`
+--
+
+CREATE TABLE `payment_methods_default` (
+  `id` int(11) UNSIGNED NOT NULL PRIMARY KEY,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `payment_methods_default`
+--
+
+INSERT INTO `payment_methods_default` (`id`, `name`) VALUES
+(1, 'Cash'),
+(2, 'Debit Card'),
+(3, 'Credit Card');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL PRIMARY KEY,
+  `username` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `expenses_category_default`
+--
+ALTER TABLE `expenses_category_default`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT dla tabeli `incomes_category_default`
+--
+ALTER TABLE `incomes_category_default`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT dla tabeli `payment_methods_default`
+--
+ALTER TABLE `payment_methods_default`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
