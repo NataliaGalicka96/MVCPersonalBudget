64e0392324d866d6c20894fe4b8541dd0343ea7e
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-19 15:19:09 +0200

    Add constructor to Category model

--- App/Models/Category.php
+++ App/Models/Category.php
@@ -6,6 +6,14 @@ use PDO;
 
 class Category extends \Core\Models
 {
+
+    public function __construct($data = [])
+    {
+        foreach($data as $key => $value) {
+            
+            $this -> $key = $value;
+        };
+    }
   
     public static function assignDefaultCategoriesToNewUser()
     {
