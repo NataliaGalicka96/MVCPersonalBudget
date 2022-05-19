76a6e582a7835d1e9f5593a3ac3550e891d5e2c4
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-19 08:26:07 +0200

    Add validate() method to Income.php model and call this method in saveIncomeToDB()

--- App/Models/Income.php
+++ App/Models/Income.php
@@ -6,6 +6,14 @@ use PDO;
 
 class Income extends \Core\Model
 {
+    /**
+     * Error messages
+     *
+     * @var array
+     */
+
+    public $errors = [];
+
 
     /**
      * Class constructor 
@@ -25,11 +33,16 @@ class Income extends \Core\Model
     /**
      * Save income with the current property values
      * 
-     * @return void
+     *  @return boolean True if the user was saved, false otherwise
      */
 
       public function saveIncomeToDB()
       {
+
+        $this->validate();
+
+        if(empty($this->errors)){
+
            $income = new Income ($_POST);
 
            $sql = 'INSERT INTO incomes
@@ -46,7 +59,46 @@ class Income extends \Core\Model
            $stmt->bindValue();
 
            $stmt->execute();
+        }
+
+        return false;
 
       }
+
+
+    /**
+     * Validate current property values, adding valiation error messages to the errors array property
+     *
+     * @return void
+     */
+
+     public function validate()
+
+     {
+         
+        //Amount must be a number, maximum 2 decimals
+        
+            if(empty($this->amount )){
+                $this->errors['errorAmount1'] = 'Amount is required';
+            }
+
+            $incomeAmount = number_format($this->amount, 2, '.','');
+            $amountArray = explode('.', $incomeAmount);
+
+            if(!is_numeric($incomeAmount) || strlen($incomeAmount) <0 ||strlen($incomeAmount) >8 || strlen($amountArray[1])>2 || strlen($amountArray[0])>6){
+
+                $this->errors['errorAmount2'] = 'Enter valid positive amount - maximum 6 integer digits and 2 decimal places.';
+        
+            }
+
+
+         //Commment max length 100 characters
+
+            if(strlen($this->comment)>100){
+                $this->errors['errorComment']= "Comment can contain up to 100 characters";
+                
+            }
+
+     }
 
 }
