274a3a7b84e1eeed2c6461ec3440e23e5a4fcab1
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-20 14:17:38 +0200

    Add method to save Expense to DB in ExpenseModel

--- App/Controllers/Expense.php
+++ App/Controllers/Expense.php
@@ -23,7 +23,6 @@ class Expense extends Authenticated
 
     }
 
-
     /**
      * Show the expense page
      * 
--- App/Models/ExpenseModel.php
+++ App/Models/ExpenseModel.php
@@ -28,5 +28,41 @@ class ExpenseModel extends \Core\Model
           };
       }
 
-      
+      /**
+       * Save new expense to database
+       * 
+       * @return boolean True if the user was saved, false otherwise
+       */
+
+       public function saveExpenseToDB()
+       {
+           //$this->validateExpenseData();
+
+           if(empty($this->errors)){
+
+            $sql = 'INSERT INTO expenses
+            VALUES (NULL, :userId, (SELECT eca.id
+            FROM expenses_category_assigned_to_users AS eca
+            WHERE eca.name = :category_name AND eca.user_id = :userId),
+            (SELECT pma.id FROM  payment_methods_assigned_to_users AS pma
+            WHERE pma.name = :paymentMethod AND pma.user_id = :userId),
+            :amount, :date, :comment)'; 
+
+            $db = static::getDBConnection();
+
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':userId', $_SESSION['user_id'], PDO::PARAM_INT);
+            $stmt->bindValue(':category_name', $this->category, PDO::PARAM_STR);
+            $stmt->bindValue(':paymentMethod', $this->payment, PDO::PARAM_STR);
+            $stmt->bindValue(':amount', $this->amount, PDO::PARAM_INT);
+            $stmt->bindValue(':date', $this->date, PDO::PARAM_STR);
+            $stmt->bindValue(':comment', $this->comment, PDO::PARAM_STR);
+ 
+            return $stmt->execute();
+         }
+ 
+         return false;
+ 
+       }
 }
