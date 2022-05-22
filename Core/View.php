7afab67d11fa2164b2f8bac028d3ef5fa591ecc7
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-22 12:59:53 +0200

    Add global variables current_date

--- Core/View.php
+++ Core/View.php
@@ -62,6 +62,7 @@ class View
             $twig = new \Twig\Environment($loader);
             $twig->addGlobal('current_user', \App\Auth::getLoggedUser());
             $twig->addGlobal('flash_messages', \App\Flash::getMessages());
+            $twig->addGlobal('current_date', \App\Date::getCurrentDate());
         }
  
         return $twig->render($template, $args);
