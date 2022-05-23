f5eb840801235ff127e73c1c4d90b99a1397a000
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-05-23 06:08:02 +0200

    Delete file balanceOfCurrentMonth.html and create new: showBalance.html

--- App/Models/BalanceModel.php
+++ App/Models/BalanceModel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use PDO;
+use \App\Date;
 
 
 class BalanceModel extends \Core\Model
@@ -16,15 +17,101 @@ class BalanceModel extends \Core\Model
       * @return void
       */
 
-      public function __construct($data = [])
-      {
+    public function __construct($data = [])
+        {
           foreach ($data as $key => $value){
               $this->$key = $value;
           };
-      }
+        }
+
+    private static function getFirstDayOfCurrentMonth()
+    {
+            $startDate = Date::getFirstDayOfCurrentMonth();
+            return $startDate;    
+    }
+
+    private static function getLastDayOfCurrentMonth()
+    {
+            $endDate = Date::getLastDayOfCurrentMonth();
+            return $endDate;    
+    }
 
 
-    
+
+    /**
+     * Get data from table of expenses
+     * 
+     * 
+     */
+
+     public static function getGroupedExpenses()
+     {
+
+        $startDate = static::getFirstDayOfCurrentMonth();
+		$endDate = static::getLastDayOfCurrentMonth();
+
+        $sql = "SELECT eca.name ,SUM(e.amount) AS sum
+        FROM expenses e
+        INNER JOIN expenses_category_assigned_to_users eca
+        ON e.expense_category_assigned_to_user_id = eca.id
+        WHERE e.user_id =:userId AND
+        e.date_of_expense BETWEEN :startDate AND :endDate
+        GROUP BY eca.name
+        ORDER BY SUM(e.amount) DESC";
 
 
-}
+        $db = static::getDBConnection();
+
+        $expenseCategoryQuery = $db -> prepare($sql);
+        $expenseCategoryQuery -> bindValue(':userId', $_SESSION['user_id'], PDO::PARAM_INT);
+        $expenseCategoryQuery -> bindValue(':startDate', $startDate, PDO::PARAM_STR);
+        $expenseCategoryQuery -> bindValue(':endDate', $endDate, PDO::PARAM_STR);
+        $expenseCategoryQuery -> execute();
+
+        return $expenseCategoryQuery -> fetchAll();
+     }
+
+     public static function  countExpenses()
+     {
+         $totalExpense = 0;
+         $expenses = static::getGroupedExpenses();
+
+         if(!empty($expenses)){
+             foreach($expenses as $expense){
+                $totalExpense +=$expense['sum'];
+             }
+         }
+
+         return $totalExpense;
+     }
+
+    /**
+     * Get data from table of incomes
+     * 
+     * 
+     */
+/*
+     public static function getAllIncomes()
+     {
+
+        $sql = "SELECT ica.name ,SUM(i.amount) AS sum
+        FROM incomes i
+        INNER JOIN incomes_category_assigned_to_users	 ica
+        ON i.income_category_assigned_to_user_id = ica.id
+        WHERE i.user_id =:userId AND
+        i.date_of_income BETWEEN :startDate AND :endDate
+        GROUP BY ica.name
+        ORDER BY SUM(i.amount)";
+
+        $db = static::getDBConnection();
+
+        $incomeCategoryQuery = $db -> prepare($sql);
+        $incomeCategoryQuery -> bindValue(':userId', $loggedUserId, PDO::PARAM_INT);
+        $incomeCategoryQuery -> bindValue(':startDate', $this -> startDate, PDO::PARAM_STR);
+        $incomeCategoryQuery -> bindValue(':endDate', $this -> endDate, PDO::PARAM_STR);
+        $incomeCategoryQuery -> execute();
+
+        return $incomeCategoryQuery -> fetchAll();
+     }
+     */
+    }
