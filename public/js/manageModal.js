cb65cb29ca73a373e00a610135e8744d59dde1e2
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-06-03 14:38:34 +0200

    Changes navbar and modal windows to bootstrap 5

--- public/js/manageModal.js
+++ public/js/manageModal.js
@@ -4,17 +4,17 @@ function manageModal() {
 
     $(document).on('click', '#editButton', function () {
 
-        $('#editCategoryName input[name="newCategoryName"]').val($(this).attr('data-category-name'));
-        $('#editCategoryName input[name="categoryOldId"]').val($(this).attr('data-category-id'));
-        $('#deleteCategory input[name="categoryOldId"]').val($(this).attr('data-category-id'));
-        console.log($(this).attr('data-category-id'));
+        $('#editCategoryName input[name="newCategoryName"]').val($(this).attr('data-bs-category-name'));
+        $('#editCategoryName input[name="categoryOldId"]').val($(this).attr('data-bs-category-id'));
+        $('#deleteCategory input[name="categoryOldId"]').val($(this).attr('data-bs-category-id'));
+        console.log($(this).attr('data-bs-category-id'));
 
     });
 
     $(document).on('click', '#deleteButton', function () {
 
-        $('#deleteCategory input[name="categoryOldId"]').val($(this).attr('data-category-id'));
-        console.log($(this).attr('data-category-id'));
+        $('#deleteCategory input[name="categoryOldId"]').val($(this).attr('data-bs-category-id'));
+        console.log($(this).attr('data-bs-category-id'));
 
     });
 }
