bf09a2aea50a76d5b7c084b7247e1c2b20d4be23
Natalia Galicka <natalia.galicka.programista@gmail.com>
2022-06-06 08:57:27 +0200

    Change name of css file

--- Core/Controller.php
+++ Core/Controller.php
@@ -79,7 +79,7 @@ abstract class Controller
 
         public function redirect($url)
         {
-            header('Location: http://' . $_SERVER['HTTP_HOST'] . $url, true, 303);
+            header('Location: https://' . $_SERVER['HTTP_HOST'] . $url, true, 303);
             exit;
         }
 
